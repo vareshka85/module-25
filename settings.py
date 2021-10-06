05c88a316cee8dd300067a9a7b4e0740b62dbd36
vareshka85 <80818030+vareshka85@users.noreply.github.com>
2021-10-06 23:41:34 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,6 @@
+link_login = 'http://petfriends1.herokuapp.com/login'
+
+valid_email = 'vareshka85@mail.ru'
+valid_password = '21041982Toll'
+
+link_chromedriver = 'D:/chrome_driver/chromedriver.exe'
